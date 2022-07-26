7b7fadfef79445b8c75552ef3c8c4691ea47ccc1
kylejow <77653395+kylejow@users.noreply.github.com>
2022-07-26 01:19:25 -0700

    Redirect output

---
+++ main.bat
@@ -0,0 +1,4 @@
+ping /t 8.8.8.8 > pings.txt
+
+:: https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/ping
+:: filename cannot be the same as command to be redirected
--- ping.bat
+++
@@ -1,3 +0,0 @@
-ping /t 8.8.8.8
-
-::https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/ping
