ba92b8c3e9b3c91916d73415026e095aa8b40e1b
kylejow <77653395+kylejow@users.noreply.github.com>
2022-07-28 02:17:04 -0700

    Extract ping

--- main.cpp
+++ main.cpp
@@ -12,23 +12,41 @@ using std::cin;
 using std::endl;
 using std::string;
 
-std::fstream& GotoLine(std::fstream& file, int num);
+std::fstream& goToLine(std::fstream& file, int num);
+int getPing(string line);
 
 int main(){
+    std::string line;
+    int ping;
     while(1){
         system("main.bat");
         std::fstream file("pings.txt");
-        std::string line;
-        GotoLine(file, 3);
+        goToLine(file, 3);
         getline(file, line);
-        cout << line << endl;
+
+        // if(line.substr(0, 7) == "Request"){
+        //     exit(0);
+        // }
+
+        if(line.substr(0, 5) == "Reply"){
+            ping = getPing(line);
+        }
+        cout << ping << endl;
     }
 }
 
-std::fstream& GotoLine(std::fstream& file, int num){
+std::fstream& goToLine(std::fstream& file, int num){
     file.seekg(std::ios::beg);
     for(int i=0; i < num - 1; ++i){
         file.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
     }
     return file;
+}
+
+int getPing(string line){
+    int i = 33;
+    while(isdigit(line[i])){
+        i++;
+    }
+    return std::stoi(line.substr(33, i-33));
 }
