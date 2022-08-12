61e987c9384bf5005ef0c7857b61abd6c693e572
kylejow <77653395+kylejow@users.noreply.github.com>
2022-08-11 21:39:39 -0700

    Added multithread interrupt

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 /*References
-seek to line    https://stackoverflow.com/questions/5207550/in-c-is-there-a-way-to-go-to-a-specific-line-in-a-text-file
-deque           https://en.cppreference.com/w/cpp/container/deque
+seek to line          https://stackoverflow.com/questions/5207550/in-c-is-there-a-way-to-go-to-a-specific-line-in-a-text-file
+deque                 https://en.cppreference.com/w/cpp/container/deque
+multithread interrupt https://stackoverflow.com/questions/41470942/stop-infinite-loop-in-different-thread
+thread constructor    https://en.cppreference.com/w/cpp/thread/thread/thread
 */
 
 #include <iostream>
 #include <windows.h>
 #include <deque>
+#include <thread>
+#include <atomic>
 
 #include "display.h"
 #include "ping.h"
@@ -16,21 +20,22 @@ using std::endl;
 using std::string;
 using std::vector;
 
-unsigned long long int getPingCount();
+void stopProgram(std::atomic_bool& stop);
 
 int main(){
+    system("clear");
     setCursor(false);
     std::string line;
     int ping, min = INT_MAX, max = INT_MIN, avg = 0, prevAvg = 0;
-    unsigned long long int sum = 0;
-    system("clear");
-
+    unsigned long long int sum = 0, count = 0;
+    
     vector<vector<string>> display(21, vector<string>(50, " "));
     std::deque<int> pingHistory(50, 0);
-    unsigned long long int pingCount = getPingCount();
 
-    unsigned long long int count = 0;
-    while(count < pingCount){
+    std::atomic_bool stop = false;
+    std::thread stopThread(stopProgram, std::ref(stop));
+    
+    while(!stop){
         ping = getPing();
         if(ping == -1){
             continue;
@@ -64,26 +69,18 @@ int main(){
         cout << "\nping: " << ping <<"     "
              << "\nmin: "  << min  <<"     "
              << "\nmax: "  << max  <<"     "
-             << "\navg: "  << avg  <<"     ";
+             << "\navg: "  << avg  <<"     "
+             << "\n\n\nPress Enter to exit.";
         clearScreen();
     }
+    stopThread.join();
     system("clear");
     setCursor(true);
     return 0;
 }
 
-unsigned long long int getPingCount(){
-    unsigned long long int numPings;
-    system("cls");
-    cout << "Number of pings: ";
-    cin >> numPings;
-    while(cin.fail()){
-        system("cls");
-        cout << "Number of pings: ";
-        cin.clear();
-        cin.ignore(256,'\n');
-        cin >> numPings;
-    }
-    system("cls");
-    return numPings;
+void stopProgram(std::atomic_bool& stop){
+    std::cin.get();
+    stop = true;
+    return;
 }
