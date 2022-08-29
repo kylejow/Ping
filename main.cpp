f9e6e027bfe5729b4b0449133ceeef09639bd47a
kylejow <77653395+kylejow@users.noreply.github.com>
2022-08-28 21:14:32 -0700

    Moved into while loop

--- main.cpp
+++ main.cpp
@@ -32,149 +32,152 @@ void stopProgram(std::atomic_bool& stop);
 void pauseAndExit(void);
 
 int main(){
-    string input;
-    string target = "NULL";
-    string type;
-    int polling = 0;
 
     while(1){
-        system("cls");
-        cout << "1. Google (8.8.8.8)\n"
-             << "2. Enter custom IP address\n"
-             << "3. Enter custom hostname\n"
-             << "4. Change polling rate (Default: 0)\n"
-             << "\n\nq to exit\n\n";
-        cin >> input;
-        if(input == "1"){
-            target = "8.8.8.8";
-            type = "ip";
-            break;
-        }else if(input == "2"){
-            while(!isIP(target)){
+        string input;
+        string target = "NULL";
+        string type;
+        int polling = 0;
+
+        while(1){
+            system("cls");
+            cout << "1. Google (8.8.8.8)\n"
+                << "2. Enter custom IP address\n"
+                << "3. Enter custom hostname\n"
+                << "4. Change polling rate (Default: 0)\n"
+                << "\n\nq to exit\n\n";
+            cin >> input;
+            if(input == "1"){
+                target = "8.8.8.8";
+                type = "ip";
+                break;
+            }else if(input == "2"){
+                while(!isIP(target)){
+                    system("cls");
+                    cout << "Enter IP address: ";
+                    cin >> target;
+                }
+                type = "ip";
+                break;
+            }else if(input == "3"){
                 system("cls");
-                cout << "Enter IP address: ";
+                cout << "Enter Hostname: ";
                 cin >> target;
+                type = "hostname";
+                break;
+            }else if(input == "4"){
+                polling = getPolling();
+            }else if(input == "q"){
+                system("cls");
+                exit(0);
+            }else{
+                continue;
             }
-            type = "ip";
-            break;
-        }else if(input == "3"){
-            system("cls");
-            cout << "Enter Hostname: ";
-            cin >> target;
-            type = "hostname";
-            break;
-        }else if(input == "4"){
-            polling = getPolling();
-        }else if(input == "q"){
-            system("cls");
-            exit(0);
-        }else{
-            continue;
         }
-    }
 
-    //create bat
-    ofstream createBat;
-    createBat.open ("main.bat");
-    createBat << "@echo off\nping /n 1 /l 1 " + target << " > pings.txt\n";
-    createBat.close();
+        //create bat
+        ofstream createBat;
+        createBat.open ("main.bat");
+        createBat << "@echo off\nping /n 1 /l 1 " + target << " > pings.txt\n";
+        createBat.close();
 
-    //verify input can be pinged and set length
-    string line;
-    int ipLength;
-    system("main.bat");
-    fstream file("pings.txt");
-    getline(file, line);
-    if(!line.empty()){
-        system("clear");
-        cout << "Could not reach " << target << ".\n\n\n";
-        file.close();
-        pauseAndExit();
-    }
-    if(type == "ip"){
-        goToLine(file, 3);
+        //verify input can be pinged and set length
+        string line;
+        int ipLength;
+        system("main.bat");
+        fstream file("pings.txt");
         getline(file, line);
-        //check secondary fail occuring from invalid ip
-        if(line.substr(15, 6) == "failed"){
+        if(!line.empty()){
             system("clear");
             cout << "Could not reach " << target << ".\n\n\n";
             file.close();
             pauseAndExit();
-        }else{
-            ipLength = target.size();
         }
+        if(type == "ip"){
+            goToLine(file, 3);
+            getline(file, line);
+            //check secondary fail occuring from invalid ip
+            if(line.substr(15, 6) == "failed"){
+                system("clear");
+                cout << "Could not reach " << target << ".\n\n\n";
+                file.close();
+                pauseAndExit();
+            }else{
+                ipLength = target.size();
+            }
+        }
+        if(type == "hostname"){
+            goToLine(file, 3);
+            getline(file, line);
+            ipLength = getIPLength(line);
+        }
+        file.close();
+
+        system("clear");
+        setCursor(false);
+        cout << std::fixed << std::setprecision(5);
+        int ping, min = INT_MAX, max = INT_MIN, avg = 0, prevAvg = 0;
+        long double jitter = 0, loss = 0;
+        unsigned long long int sum = 0, count = 0;
+
+        vector<vector<string>> display(21, vector<string>(50, " "));
+        std::deque<int> pingHistory(50, 0);
+
+        std::atomic_bool stop = false;
+        thread stopThread(stopProgram, ref(stop));
+        while(!stop){
+            thread removePoints(setAllPoints, ref(pingHistory), ref(display), prevAvg, " ");
+            ping = getPing(ipLength);
+            removePoints.join();
+
+            if(ping == 0){
+                loss++;
+                continue;
+            }
+
+            if(ping > max){
+                max = ping;
+            }
+            if(ping < min){
+                min = ping;
+            }
+            sum += ping;
+            count++;
+            avg = sum/count;
+            pingHistory.push_back(ping);
+
+            if(avg != prevAvg){
+                prevAvg = avg;
+                updateAxis(display, avg);
+            }
+
+            jitter -= abs(pingHistory[1] - pingHistory[0]);
+            pingHistory.pop_front();    // shift queue
+            jitter += abs(pingHistory[49] - pingHistory[48]);
+
+            setAllPoints(pingHistory, display, prevAvg, "*");  // replace points
+
+            cout << "Pinging " << target << "\n\n";
+            printDisplay(display);
+            
+            cout << "\nPing: "   << ping <<"     "
+                << "\nMin: "    << min  <<"     "
+                << "\nMax: "    << max  <<"     "
+                << "\nAvg: "    << avg  <<"     "
+                << "\nJitter: " << jitter/49  <<"     "
+                << "\nPacket Loss: " << loss/count*100  <<"%     "
+                << "\n\n\nPress Enter to exit.";
+
+            Sleep(polling);
+            clearScreen();
+        }
+
+        stopThread.join();
+        system("clear");
+        system("rm pings.txt");
+        system("rm main.bat");
+        setCursor(true);
     }
-    if(type == "hostname"){
-        goToLine(file, 3);
-        getline(file, line);
-        ipLength = getIPLength(line);
-    }
-    file.close();
-
-    system("clear");
-    setCursor(false);
-    cout << std::fixed << std::setprecision(5);
-    int ping, min = INT_MAX, max = INT_MIN, avg = 0, prevAvg = 0;
-    long double jitter = 0, loss = 0;
-    unsigned long long int sum = 0, count = 0;
-
-    vector<vector<string>> display(21, vector<string>(50, " "));
-    std::deque<int> pingHistory(50, 0);
-
-    std::atomic_bool stop = false;
-    thread stopThread(stopProgram, ref(stop));
-    while(!stop){
-        thread removePoints(setAllPoints, ref(pingHistory), ref(display), prevAvg, " ");
-        ping = getPing(ipLength);
-        removePoints.join();
-
-        if(ping == 0){
-            loss++;
-            continue;
-        }
-
-        if(ping > max){
-            max = ping;
-        }
-        if(ping < min){
-            min = ping;
-        }
-        sum += ping;
-        count++;
-        avg = sum/count;
-        pingHistory.push_back(ping);
-
-        if(avg != prevAvg){
-            prevAvg = avg;
-            updateAxis(display, avg);
-        }
-
-        jitter -= abs(pingHistory[1] - pingHistory[0]);
-        pingHistory.pop_front();    // shift queue
-        jitter += abs(pingHistory[49] - pingHistory[48]);
-
-        setAllPoints(pingHistory, display, prevAvg, "*");  // replace points
-
-        cout << "Pinging " << target << "\n\n";
-        printDisplay(display);
-        
-        cout << "\nPing: "   << ping <<"     "
-             << "\nMin: "    << min  <<"     "
-             << "\nMax: "    << max  <<"     "
-             << "\nAvg: "    << avg  <<"     "
-             << "\nJitter: " << jitter/49  <<"     "
-             << "\nPacket Loss: " << loss/count*100  <<"%     "
-             << "\n\n\nPress Enter to exit.";
-
-        Sleep(polling);
-        clearScreen();
-    }
-
-    stopThread.join();
-    system("clear");
-    system("rm pings.txt");
-    system("rm main.bat");
-    setCursor(true);
     return 0;
 }
 
